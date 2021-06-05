47d13b6532806ab6313b3fb4dfad64423766533b
Neb <pintaricn@gmail.com>
2021-06-05 02:45:33 +0200

    transformations to overcome JS slowers

--- src/transform.ts
+++ src/transform.ts
@@ -2,6 +2,7 @@ import cheerio from 'cheerio'
 import Response from '@layer0/core/router/Response'
 import Request from '@layer0/core/router/Request'
 import { injectBrowserScript } from '@layer0/starter'
+import { parse } from 'path'
 
 export default function transform(response: Response, request: Request) {
   // inject browser.ts into the document returned from the origin
@@ -18,17 +19,53 @@ export default function transform(response: Response, request: Request) {
       <script src="/main.js" defer="defer"></script>
     `)
 
-    // your transformations go here
-    // ...
-    // ...
-
     $('a[href^="https://www.mcafee.com"]').map((i, el) => {
       var link = $(el).attr('href') || '';
       $(el).attr('href', link.replace(/.+\.com\//, '/'));
     })
 
+    $('.cmp-loader').remove()
+
+    $('#banner .bg-stretch').map((i, el) => {
+      var imageURL = $(el).children('img').attr('src');
+      var style = `background-image: url("${imageURL}"); background-size: cover; background-position: center center; background-repeat: no-repeat;`
+      $(el).attr('style', style);
+    })
+
+    $('v-lazy-image').slice(0,3).map((i, el) => {
+      var imageURL = $(el).attr('src');
+      $(el).parent().append(`<img class="v-lazy-image v-lazy-image-loaded" src="${imageURL}"/>`)
+      $(el).remove()
+    })
+
+    var price = '',
+        savings = '',
+        retailPrice = '';
+
+    $('#mtp-title + p + p[v-if^="digitalData.offersGroup.dropdownSelected =="]').remove()
+
+    $('script:contains("var digitalData = {")').map((i, el) => {
+      var scriptRaw = $(el).html() || '';
+      var script = scriptRaw.replace('var digitalData = ','')
+      var scriptJSON = JSON.parse(script)
+
+      var priceRaw = script.match(/digitalData.offers.o3.finalPriceAfterCashback=\\"([^\\]+)/) || '';
+      price = priceRaw[1]
+
+      var savingsRaw = script.match(/digitalData.offers.o3.discountAmountAfterCashback=\\"([^\\]+)/) || '';
+      savings = savingsRaw[1]
+
+      var retailPriceRaw = script.match(/digitalData.offers.o3.retailPrice=\\"([^\\]+)/) || '';
+      retailPrice = retailPriceRaw[1]
+
+    })
+
+
     response.body = $.html()
                       .replace(/https:\/\/www\.mcafee\.com\//g, '/')
+                      .replace("{{finalPriceAfterCashback(digitalData.offersGroup.dropdownSelected)}}", price)
+                      .replace("{{retailPrice(digitalData.offersGroup.dropdownSelected)}}", retailPrice)
+                      .replace("{{discountedAmountAfterCashback(digitalData.offersGroup.dropdownSelected)}}", savings)
 
   }
 }
